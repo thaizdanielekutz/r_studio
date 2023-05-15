8c49d50559bc9d28f97752925d9612768b2f6ad7
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-15 18:27:42 -0300

    box-plot

--- variáveis quantitativas - parte 1 - gráficos.R
+++ variáveis quantitativas - parte 1 - gráficos.R
@@ -20,6 +20,39 @@ require(tidyverse)
 
 view(iris)
 dados=iris
+grafico_hist = ggplot(dados, aes(x=Sepal.Length))+
+  geom_histogram(color= "black", fill="lightblue", bins = 30)+
+  ggtitle("Gráfico histograma do tamanho da Sépala das Plantas")+
+  xlab("Tamanho das Sépalas")+
+  ylab("Frequência simples")+
+  theme()
+grafico_hist
+ggplotly(grafico_hist)
+
+#Gráfico Box-Plot
+dados=iris
+grafico_boxplot = ggplot(dados, aes(x=Sepal.Length, y=Petal.Length, fill=Species))+
+  geom_boxplot()+
+  ggtitle("Gráfico de Box-plot do tamanho da Sépala vs o tamanho das Pétalas das plantas")+
+  xlab("Tamanho das Sépalas")+
+  ylab("Tamanho das Pétalas")
+grafico_boxplot
+
+
+#Gráfico de dispersão - cor espécies
+dados=iris
+grafico_dispersao_cor = ggplot(dados, aes(x=Sepal.Length, y = Petal.Length, col=Species))+
+  geom_point(size=4)+
+  ggtitle("Gráfico de Dispersão do tamanho da Sépala vs o tamanho das Pétalas das plantas")+
+  xlab("Tamanho das Sépalas")
+  ylab("Tamanho das Pétalas")
+grafico_dispersao_cor
+  
+  
+
+
+
+
 
 
 
