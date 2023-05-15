fe128e6293da191447fe4f4135d9735f060ec5e1
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-05-15 12:16:36 -0300

    graficohist

---
+++ variáveis quantitativas - parte 1 - gráficos.R
@@ -0,0 +1,26 @@
+# variáveis Quantitativas
+require(ggplot2)
+require(dplyr)
+require(scales)
+require(plotly)
+require(gridExtra)
+require(tidyverse)
+
+#ggplot geral
+#Elementos de um gráfico ggplot
+
+#1. Base de dados que será utilizada
+#2. Aesthetic mapping (A parte estética do gráfico, eixos, cores, tamanhos)
+#3. Geometria que será utilizada (Tipo de gráfico)
+#4. Escala (Formato, unidade de medida)
+#5. Rótulos, títulos, legendas, etc...
+
+
+#Gráfico Histograma
+
+view(iris)
+dados=iris
+
+
+
+
