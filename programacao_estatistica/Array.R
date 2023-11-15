c3bc9ae4bcfcb212fab6bc9b0536e377d97129ef
thaizdanielekutz <122176055+thaizdanielekutz@users.noreply.github.com>
2023-11-15 08:36:25 -0300

    matriz e array

---
+++ programacao_estatistica/Array.R
@@ -0,0 +1,6 @@
+#ARRAY
+# Criando um array 3D com dimensões 2x3x4
+meuArray <- array(data = 1:24, dim = c(2, 3, 4))
+
+# Exibindo o array
+print(meuArray)
---
+++ programacao_estatistica/Matriz.R
@@ -0,0 +1,12 @@
+#MATRIZ
+# Substitua "valores a preencher a matriz" pelos valores que você deseja preencher na matriz
+# Substitua 'nrow' pelo número desejado de linhas
+# Substitua 'ncol' pelo número desejado de colunas
+
+matriz <- matrix(data = c("valores a preencher a matriz"), nrow = 2, ncol = 3)
+
+# Exemplo com valores concretos:
+matriz_exemplo <- matrix(data = c(1, 2, 3, 4, 5, 6), nrow = 2, ncol = 3)
+
+# Exibindo a matriz
+print(matriz_exemplo)
